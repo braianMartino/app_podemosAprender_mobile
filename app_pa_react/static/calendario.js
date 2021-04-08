2ee84b7ee0d3f3d2b12e164552f09a7cd0186eea
braian <braian.martino@gmail.com>
2021-04-08 14:20:04 -0300

    LIMPIEZA

---
+++ app_pa_react/static/calendario.js
@@ -0,0 +1,33 @@
+async function apiFavoritos() {
+  const res = await fetch("http://127.0.0.1:8000/api/favorito/");
+  const data = await res.json();
+  return data.results;
+}
+
+async function obtenerListaFavoritos() {
+  const idFavoritos = await apiFavoritos(); //A: Traigo los IDs de las tarjetas faveadas y quien las faveo
+  const tarjetas = await apiTarjetasDict(); //A: Traigo todas las tarjetas de la API
+  let listaFavoritos = [];
+  idFavoritos.forEach((f) => { //A: solo carga favoritos si coinciden el campo de_quien de api/favorito con el id que se pase por parametro
+    //que deberia ser el id del USUARIO LOGU(EADO
+    if ( f.de_quien == idUsuarioLogeado ) {//A: le paso directamente la variable que declare y cargo en myLogin
+      listaFavoritos.push(tarjetas[f.lugar]);
+    }//TODO: escribir más corto?
+  }) 
+  //DBG:console.log(idUsuarioLogeado, listaFavoritos)
+  return listaFavoritos;
+}
+
+var Calendario = React.createClass({
+  render: function() {
+    return (
+      <Ons.Page renderToolbar={this.renderToolbar}>  
+          <Cards funcionTarjetas={obtenerListaFavoritos} usuarioId={this.props.usuarioId}/>
+      </Ons.Page>
+    );
+  }
+});
+
+
+  
+
---
+++ app_pa_react/static/miCuenta.js
@@ -0,0 +1,34 @@
+var MiCuenta = React.createClass({
+
+  createMarkup: function() {
+    return {__html: marked(this.props.texto)};
+  },
+
+
+
+  render: function() {
+    return (
+      <Ons.Page>
+        <section style={{ margin: "16px" }}>
+            <div dangerouslySetInnerHTML={this.createMarkup()} />;
+        </section>
+      </Ons.Page>
+    );
+  }
+});
+
+//Fetch para traer lista de usuarios y listarlos
+//Seguí ejemplo de aca: https://onsen.io/v2/guide/tutorial.html#remote-api-calls
+
+let url = "https://si.podemosaprender.org/api/participante/";
+
+const listarUsuarios = async () => {
+  // do the API call and get JSON response
+  const response = await fetchConToken(url);
+  const json = await response.json();
+
+  const usuarios = json.results.map(e => e.username);
+  return usuarios;
+  
+};
+listarUsuarios();
